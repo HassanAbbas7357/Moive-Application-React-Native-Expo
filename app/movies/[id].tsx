662e2321fe9f3ee449f6dfc8c92712f14ffe743e
hassan-repliocaide <ah735752@gmail.com>
2025-11-13 11:18:50 +0500

    final commit

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,10 +1,71 @@
-import { View, Text } from 'react-native';
+import { icons } from '@/constants/icons';
+import { fetchMovieDetails } from '@/services/api';
+import useFetch from '@/services/useFetch';
+import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
+import { Image, ScrollView, Text, View } from 'react-native';
+
+interface MovieInfoProps {
+  label: string;
+  value: string | number | null;
+}
+
+const MovieInfo = ({ label, value }: MovieInfoProps) => (
+  <View className='flex-col items-start justify-center mt-5'>
+    <Text className='text-light-200 font-normal text-sm'>{label}</Text>
+    <Text className='text-light-100 font-bold text-sm mt-2'>{value || 'N/A'}</Text>
+  </View>
+);
 
 const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+  const { data: movie, loading } = useFetch(() => fetchMovieDetails(id as string));
   return (
-    <View>
-      <Text>MovieDetails</Text>
+    <View className='bg-primary flex-1'>
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{ uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}` }}
+            className='w-full h-[550px]'
+            resizeMode='stretch'
+          />
+        </View>
+        <View className='flex-col items-start justify-center mt-5 px-5'>
+          <Text className='text-white font-bold text-xl'>{movie?.title}</Text>
+          <View className='flex-row items-center gap-x-1 mt-2'>
+            <Text className='text-light-200 text-sm'>{movie?.release_date?.split('-')[0]} •</Text>
+            <Text className='text-light-200 text-sm'>{movie?.runtime}m</Text>
+          </View>
+
+          <View className='flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2'>
+            <Image source={icons.star} className='size-4' />
+            <Text className='text-white font-bold tex-sm'>
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+            <Text className='text-light-200 text-sm'> ({movie?.vote_count} votes)</Text>
+          </View>
+
+          <MovieInfo label='Overview' value={movie?.overview || 'N/A'} />
+          <MovieInfo label='Genres' value={movie?.genres?.map(g => g.name).join(' - ') || 'N/A'} />
+          <View className='flex-row justify-between w-1/2'>
+            <MovieInfo
+              label='Budget'
+              value={movie?.budget ? `$${Math.round(movie.budget / 1_000_000)} million` : 'N/A'}
+            />
+            <MovieInfo
+              label='Revenue'
+              value={
+                !movie?.revenue
+                  ? 'N/A'
+                  : movie.revenue < 1_000_000
+                    ? `$${Math.round(movie.revenue / 1_000).toFixed(0)}K`
+                    : `$${Math.round(movie.revenue / 1_000_000)} million`
+              }
+            />
+          </View>
+          <MovieInfo label='Production Companies' value={!movie?.production_companies.length? 'N/A': movie.production_companies.map((c)=>c.name).join(" - ")} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
